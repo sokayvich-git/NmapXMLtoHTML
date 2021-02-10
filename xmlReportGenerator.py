30325a7d0fd82bb0eed8c26950e6f84445298119
sokayvich-git <57150013+sokayvich-git@users.noreply.github.com>
2021-02-10 17:26:20 -0500

    Update xmlReportGenerator.py
    Fixed some directory weirdness

--- xmlReportGenerator.py
+++ xmlReportGenerator.py
@@ -3,7 +3,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--directory", help="The parent directory containing the nmap xml file want to combine.", type=str,
-                    default='.')
+                    default='./')
 parser.add_argument("-x", "--xmldocument", help="The name of the xml document that will be finally converted to xml.",
                     type=str, default='combined.xml')
 parser.add_argument("-f", "--htmldocument", help="The name of the html document that will be the final report.",
@@ -13,7 +13,10 @@ parser.add_argument("-b", "--browser", help="The browser that you might want to 
 args = parser.parse_args()
 
 
-def combine(directory='.', finaldoc='combined.xml', htmldoc='combined.html', browser='firefox'):
+def combine(directory='./', finaldoc='combined.xml', htmldoc='combined.html', browser='firefox'):
+    if directory[-1] != '/':
+        directory += '/'
+
     dirlist = os.listdir(directory)
     filecount = 0
     
@@ -26,10 +29,10 @@ def combine(directory='.', finaldoc='combined.xml', htmldoc='combined.html', bro
             continue
         elif '.xml' in x[-4:]:
             filecount += 1
-            print(f'Adding {directory}/{x}')
-            f = open(f'{directory}/{x}', 'r')
+            print(f'Adding {directory}{x}')
+            f = open(f'{directory}{x}', 'r')
             lines = f.readlines()
-            doc = open(f'{directory}/{finaldoc}', 'a')
+            doc = open(f'{directory}{finaldoc}', 'a')
             for line in lines:
                 if (filecount != 1) and (line in lines[:5]):
                     continue
@@ -38,17 +41,17 @@ def combine(directory='.', finaldoc='combined.xml', htmldoc='combined.html', bro
             doc.close()
             f.close()
         else:
-            print(f'Skipping {directory}/{x}')
+            print(f'Skipping {directory}{x}')
 
     if finaldoc in os.listdir(directory):
-        doc = open(finaldoc, 'a')
+        doc = open(f'{directory}{finaldoc}', 'a')
         doc.write("</nmaprun>")
         doc.close()
-        print(f"Generating {directory}/{htmldoc}")
-        os.system(f"xsltproc {directory}/{finaldoc} -o {directory}/{htmldoc}")
+        print(f"Generating {directory}{htmldoc}")
+        os.system(f"xsltproc {directory}{finaldoc} -o {directory}{htmldoc}")
         answer = input(f'Do you want to view in {browser}? (yes/no) ')
         if answer.lower() == 'yes':
-            os.system(f'{browser} {directory}/{htmldoc}')
+            os.system(f'{browser} {directory}{htmldoc}')
     else:
         print('No files combined. Exiting...')
         exit(1)
